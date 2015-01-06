fc6e9c45d9738fc86cf58196f8bb11cfb615c8e5
Kang In Cheol <kangic21@gmail.com>
2015-01-06 20:06:07 +0900

    delete some lines in init.el

--- init.el
+++ init.el
@@ -106,47 +106,8 @@
 	  (lambda ()
 	    (gtags-mode 1)))
 
-;; create tag files if tag files doesn't exist
-(defun gtags-create-or-update ()
-  "create or update the gnu global tag file"
-  (interactive)
-  (if (not (= 0 (call-process "global" nil nil nil " -p"))) ; tagfile doesn't exist?
-      (let ((olddir default-directory)
-            (topdir (read-directory-name
-                     "gtags: top of source tree:" default-directory)))
-        (cd topdir)
-        (shell-command "gtags && echo 'created tagfile'")
-        (cd olddir)) ; restore
-    ;;  tagfile already exists; update it
-    (shell-command "global -u && echo 'updated tagfile'")))
-
-(add-hook 'c-mode-common-hook
-          (lambda ()
-            (gtags-create-or-update)))
-
 ;; short-key bindings
 (add-hook 'gtags-mode-hook
           (lambda ()
             (local-set-key (kbd "M-.") 'gtags-find-tag)
             (local-set-key (kbd "M-,") 'gtags-find-rtag)))
-
-;; auto-update tag file when changed that file
-(defun gtags-update-single (filename)
-  "Update Gtags database for changes in a single file"
-  (interactive)
-  (start-process "update-gtags" "update-gtags" "bash" "-c" (concat "cd " (gtags-root-dir) " ; gtags --single-update " filename )))
-
-(defun gtags-update-current-file()
-  (interactive)
-  (defvar filename)
-  (setq filename (replace-regexp-in-string (gtags-root-dir) "." (buffer-file-name (current-buffer))))
-  (gtags-update-single filename)
-  (message "Gtags updated for %s" filename))
-
-(defun gtags-update-hook()
-  "Update GTAGS file incrementally upon saving a file"
-  (when gtags-mode
-    (when (gtags-root-dir)
-      (gtags-update-current-file))))
-
-(add-hook 'after-save-hook 'gtags-update-hook)
