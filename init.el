5701fbebc2ea93dae8631be9adb04902e449502c
Kang In Cheol <kangic21@gmail.com>
2015-06-03 13:29:27 +0900

    add some global settings

--- init.el
+++ init.el
@@ -1,29 +1,35 @@
-; start package.el with emacs
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; start package.el with emacs
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 (package-initialize)
 
-; maximize
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; maximize
 ;(add-to-list 'default-frame-alist '(fullscreen . maximized))
 
-; org-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; org-mode
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
 
 (setq org-agenda-files (list "~/Dropbox/notes/tasks/todo.org"))
 
-; start auto-complete with emacs
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; start auto-complete with emacs
 (require 'auto-complete)
 ; do default config for auto-compelete
 (require 'auto-complete-config)
 (ac-config-default)
 
-; start yasnippet with emacs
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; start yasnippet with emacs
 (require 'yasnippet)
 (yas-global-mode 1)
 
-; auto-compelete for c/c++
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; auto-compelete for c/c++
 (defun my:ac-c-header-init()
   (require 'auto-complete-c-headers)
   (add-to-list 'ac-sources 'ac-source-c-headers)
@@ -32,25 +38,34 @@
 (add-hook 'c++-mode-hook 'my:ac-c-header-init)
 (add-hook 'c-mode-hook 'my:ac-c-header-init)
 
-; iedit-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; iedit-mode
 (require 'iedit)
 (define-key global-map (kbd "C-c ;") 'iedit-mode)
 
-; ido-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ido-mode
 (require 'ido)
 (ido-mode t)
 
-; turn on Semantic
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; turn on Semantic
 (semantic-mode 1)
 (defun my:add-semantic-to-autocomplete()
   (add-to-list 'ac-sources 'ac-source-semantic)
 )
 (add-hook 'c-mode-common-hook 'my:add-semantic-to-autocomplete)
-; turn on ede mode
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; turn on ede mode
 (global-ede-mode 1)
-; turn on automatic reparsing of open buffer in semantic
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; turn on automatic reparsing of open buffer in semantic
 (global-semantic-idle-scheduler-mode 1)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; project
 (ede-cpp-root-project "eznetpp"
                 :name "eznetpp library"
                 :file "~/works/github/eznetpp/CMakeLists.txt"
@@ -59,43 +74,53 @@
                                )
                 :system-include-path '("/usr/include"))
 
-; color-theme
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; color-theme
 (load-theme 'wombat t)
 
-; tool-bar / scroll-bar off
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; tool-bar / scroll-bar off
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
 
-; line-number-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; line-number-mode
 (global-linum-mode t)
 
 (show-paren-mode 1)
 (setq show-paren-delay 0)
 
-; column-number-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; column-number-mode
 (setq column-number-mode t)
 
-; column-width
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; column-width
 (setq-default fill-column 80)
 
-; alias to asnwer yes or no
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; alias to asnwer yes or no
 (defalias 'yes-or-no-p 'y-or-n-p)
 
-; don't save backup files
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; don't save backup files
 (setq make-backup-files nil)
 
-; hangul
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; hangul
 (set-language-environment "Korean")
 (prefer-coding-system 'utf-8)
 (global-set-key (kbd "<S-kana>") 'toggle-input-method)
 
-; font
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; font
 (set-face-font 'default "Bitstream Vera Sans Mono-12")
 (set-fontset-font "fontset-default" '(#x1100 . #xffdc)
                    "NanumGothic_Coding-15")
 
-; flymake-google-cpplint
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; flymake-google-cpplint
 (require 'flymake-google-cpplint)
 
 (add-hook 'c-mode-hook 'flymake-google-cpplint-load)
@@ -117,15 +142,18 @@
  '(help-at-pt-time-delay 0.9)
  '(org-agenda-files (quote ("~/datas/repository/Dropbox/notes/tasks/todo.org"))))
 
-; google-c-style
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; google-c-style
 (require 'google-c-style)
 (add-hook 'c-mode-common-hook 'google-set-c-style)
 (add-hook 'c-mode-common-hook 'google-make-newline-indent)
 
-; flymake-cursor
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; flymake-cursor
 (require 'flymake-cursor)
 
-; markdown-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; markdown-mode
 (autoload 'markdown-mode "markdown-mode"
   "Major mode for editing Markdown files" t)
 
@@ -134,11 +162,13 @@
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
 
-; change short key for ibuffer
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; change short key for ibuffer
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (autoload 'ibuffer "ibuffer" "List buffers." t)
 
-; sr-speedbar
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; sr-speedbar
 (require 'sr-speedbar)
 
 (setq sr-speedbar-right-side nil)
@@ -154,19 +184,58 @@
 
 (global-set-key (kbd "C-x p") 'my:toggle-speedbar)
 
-; switch to header file
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; switch to header file
 (defun my:switch-to-headerfile()
   (global-set-key (kbd "C-c o") 'ff-find-other-file)
 )
 (add-hook 'c-mode-hook 'my:switch-to-headerfile)
 (add-hook 'c++-mode-hook 'my:switch-to-headerfile)
 
-; for Global(sudo apt-get install global)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; for Global(sudo apt-get install global)
 (setq load-path (cons "/usr/share/emacs24/site-lisp/global/gtags.el" load-path))
 (autoload 'gtags-mode "gtags" "" t)
 (add-hook 'c-mode-common-hook
   (lambda ()
     (gtags-mode 1)))
+
+(defun gtags-create-or-update ()
+  "create or update the gnu global tag file"
+  (interactive)
+  (if (not (= 0 (call-process "global" nil nil nil " -p"))) ; tagfile doesn't exist?
+      (let ((olddir default-directory)
+            (topdir (read-directory-name
+                     "gtags: top of source tree:" default-directory)))
+        (cd topdir)
+        (shell-command "gtags && echo 'created tagfile'")
+        (cd olddir)) ; restore
+    ;;  tagfile already exists; update it
+    (shell-command "global -u && echo 'updated tagfile'")))
+
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (gtags-create-or-update)))
+
+(defun gtags-update-single (filename)
+  "Update Gtags database for changes in a single file"
+  (interactive)
+  (start-process "update-gtags" "update-gtags" "bash" "-c" (concat "cd " (gtags-root-dir) " ; gtags --single-update " filename )))
+
+(defun gtags-update-current-file()
+  (interactive)
+  (defvar filename)
+  (setq filename (replace-regexp-in-string (gtags-root-dir) "." (buffer-file-name (current-buffer))))
+  (gtags-update-single filename)
+  (message "Gtags updated for %s" filename))
+
+(defun gtags-update-hook()
+  "Update GTAGS file incrementally upon saving a file"
+  (when gtags-mode
+    (when (gtags-root-dir)
+      (gtags-update-current-file))))
+
+(add-hook 'after-save-hook 'gtags-update-hook)
 
 ;; short-key bindings
 (add-hook 'gtags-mode-hook
@@ -174,9 +243,11 @@
 	    (local-set-key (kbd "M-.") 'gtags-find-tag)
             (local-set-key (kbd "M-,") 'gtags-find-rtag)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; move-window by meta key
 (windmove-default-keybindings 'meta)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; revive
 (require 'revive)
 (autoload 'save-current-configuration "revive" "Save status" t)
@@ -193,19 +264,23 @@
  ;; If there is more than one, they won't work right.
  )
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; x-clipboard paste
 (setq x-select-enable-clipboard t)
 (setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; set transparency
 ;(set-frame-parameter nil 'alpha '(80 70))
 (set-background-color "black")
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; magit
 (require 'magit)
 (setq magit-auto-revert-mode nil)
 (setq magit-last-seen-setup-instructions "1.4.0")
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; helm
 (require 'helm-config)
 (global-set-key (kbd "M-x") 'helm-M-x)
@@ -213,5 +288,6 @@
 (helm-mode 1)
 (helm-autoresize-mode 1)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; scheme
 (set-variable (quote scheme-program-name) "scheme")
